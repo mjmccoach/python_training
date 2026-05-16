2ce5700e04da2daf788f636c6ce2b5850f0e4788
Matthew <m.mccoach88@gmail.com>
2026-05-16 23:53:22 +0100

    Add single class labs.

---
+++ section_13/team_class/src/team.py
@@ -0,0 +1,16 @@
+class Team:
+    def __init__(self, name, players, coach):
+        self.name = name
+        self.players = players
+        self.coach = coach
+        self.points = 0
+    
+    def add_player(self, player):
+        self.players.append(player)
+    
+    def has_player(self, player):
+        return player in self.players
+    
+    def play_game(self, has_won):
+        if has_won == True:
+            self.points +=3
